ad375f2a48c89ecff2845af97495b993418808b6
davidfung <davidfung@amgcomputing.com>
2022-08-22 23:27:24 -0700

    Next 17.4 - Parsing Prefix Expressions

--- compiler.c
+++ compiler.c
@@ -13,6 +13,12 @@ typedef struct
 } Parser;
 
 Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk()
+{
+    return compilingChunk;
+}
 
 static void errorAt(Token *token, const char *message)
 {
@@ -65,6 +71,7 @@ static void advance()
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
+    compilingChunk = chunk;
 
     parser.hadError = false;
     parser.panicMode = false;
@@ -72,5 +79,27 @@ bool compile(const char *source, Chunk *chunk)
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
+}
+
+static void emitByte(uint8_t byte)
+{
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler()
+{
+    emitReturn();
 }
