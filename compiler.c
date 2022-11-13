d08ddff6155e576e5ed138a754f81d771513a909
davidfung <davidfung@amgcomputing.com>
2022-11-12 17:19:48 -0800

    Next 21.1.2

--- compiler.c
+++ compiler.c
@@ -108,6 +108,23 @@ static void consume(TokenType type, const char *message)
     errorAtCurrent(message);
 }
 
+// The check() function returns true if the current token has
+// the given type.
+static bool check(TokenType type)
+{
+    return parser.current.type == type;
+}
+
+// If the current token has the given type, we consume the
+// token and return true.
+static bool match(TokenType type)
+{
+    if (!check(type))
+        return false;
+    advance();
+    return true;
+}
+
 static void emitByte(uint8_t byte)
 {
     writeChunk(currentChunk(), byte, parser.previous.line);
@@ -153,6 +170,8 @@ static void endCompiler()
 }
 
 static void expression();
+static void statement();
+static void declaration();
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -329,6 +348,25 @@ static void expression()
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void printStatement()
+{
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+    emitByte(OP_PRINT);
+}
+
+static void declaration()
+{
+    statement();
+}
+
+static void statement()
+{
+    if (match(TOKEN_PRINT))
+    {
+        printStatement();
+    }
+}
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
@@ -338,8 +376,12 @@ bool compile(const char *source, Chunk *chunk)
     parser.panicMode = false;
 
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+
+    while (!match(TOKEN_EOF))
+    {
+        declaration();
+    }
+
     endCompiler();
     return !parser.hadError;
 }
