0bc2b3bfc0470bd0c9c54a6c8f69aef8cf38ad9c
davidfung <davidfung@amgcomputing.com>
2023-03-28 16:33:40 -0700

    Next 26.3

--- memory.c
+++ memory.c
@@ -3,8 +3,19 @@
 #include "memory.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize)
 {
+      if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+    collectGarbage();
+#endif
+  }
+
     if (newSize == 0)
     {
         free(pointer);
@@ -19,6 +30,10 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize)
 
 static void freeObject(Obj *object)
 {
+#ifdef DEBUG_LOG_GC
+  printf("%p free type %d\n", (void*)object, object->type);
+#endif
+
     switch (object->type)
     {
     case OBJ_CLOSURE:
@@ -50,6 +65,17 @@ static void freeObject(Obj *object)
         FREE(ObjUpvalue, object);
         break;
     }
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+  printf("-- gc begin\n");
+#endif
+
+
+#ifdef DEBUG_LOG_GC
+  printf("-- gc end\n");
+#endif
 }
 
 void freeObjects()
