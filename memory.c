77c5247ffc7bb1be6d1dd2f032ad3bab8b4c0b5b
davidfung <davidfung@amgcomputing.com>
2023-04-03 20:14:39 -0700

    Next 26.4.3

--- memory.c
+++ memory.c
@@ -11,11 +11,12 @@
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize)
 {
-      if (newSize > oldSize) {
+    if (newSize > oldSize)
+    {
 #ifdef DEBUG_STRESS_GC
-    collectGarbage();
+        collectGarbage();
 #endif
-  }
+    }
 
     if (newSize == 0)
     {
@@ -29,14 +30,16 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize)
     return result;
 }
 
-void markValue(Value value) {
-    if (IS_OBJ(value)) markObject(AS_OBJ(value));
+void markValue(Value value)
+{
+    if (IS_OBJ(value))
+        markObject(AS_OBJ(value));
 }
 
 static void freeObject(Obj *object)
 {
 #ifdef DEBUG_LOG_GC
-    printf("%p free type %d\n", (void*)object, object->type);
+    printf("%p free type %d\n", (void *)object, object->type);
 #endif
 
     switch (object->type)
@@ -72,36 +75,54 @@ static void freeObject(Obj *object)
     }
 }
 
-void markObject(Obj* object) {
-    if (object == NULL) return;
+void markObject(Obj *object)
+{
+    if (object == NULL)
+        return;
 #ifdef DEBUG_LOG_GC
-    printf("%p mark ", (void*)object);
+    printf("%p mark ", (void *)object);
     printValue(OBJ_VAL(object));
     printf("\n");
 #endif
 
     object->isMarked = true;
+
+    if (vm.grayCapacity < vm.grayCount + 1)
+    {
+        vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
+        vm.grayStack = (Obj **)realloc(vm.grayStack,
+                                       sizeof(Obj *) * vm.grayCapacity);
+        if (vm.grayStack == NULL)
+            exit(1);
+    }
+
+    vm.grayStack[vm.grayCount++] = object;
 }
 
-static void markRoots() {
-    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+static void markRoots()
+{
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+    {
         markValue(*slot);
     }
 
-    for (int i = 0; i < vm.frameCount; i++) {
-        markObject((Obj*)vm.frames[i].closure);
+    for (int i = 0; i < vm.frameCount; i++)
+    {
+        markObject((Obj *)vm.frames[i].closure);
     }
 
-    for (ObjUpvalue* upvalue = vm.openUpvalues;
-            upvalue != NULL;
-            upvalue = upvalue->next) {
-        markObject((Obj*)upvalue);
+    for (ObjUpvalue *upvalue = vm.openUpvalues;
+         upvalue != NULL;
+         upvalue = upvalue->next)
+    {
+        markObject((Obj *)upvalue);
     }
     markTable(&vm.globals);
     markCompilerRoots();
 }
 
-void collectGarbage() {
+void collectGarbage()
+{
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
 #endif
@@ -109,7 +130,7 @@ void collectGarbage() {
     markRoots();
 
 #ifdef DEBUG_LOG_GC
-  printf("-- gc end\n");
+    printf("-- gc end\n");
 #endif
 }
 
@@ -122,4 +143,6 @@ void freeObjects()
         freeObject(object);
         object = next;
     }
+
+    free(vm.grayStack);
 }
