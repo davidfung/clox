de76af7e638dc47431afdc04be35e8806c6f7e1f
davidfung <davidfung@amgcomputing.com>
2023-04-03 20:24:52 -0700

    Next 26.5

--- memory.c
+++ memory.c
@@ -36,6 +36,50 @@ void markValue(Value value)
         markObject(AS_OBJ(value));
 }
 
+static void markArray(ValueArray *array)
+{
+    for (int i = 0; i < array->count; i++)
+    {
+        markValue(array->values[i]);
+    }
+}
+
+static void blackenObject(Obj *object)
+{
+#ifdef DEBUG_LOG_GC
+    printf("%p blacken ", (void *)object);
+    printValue(OBJ_VAL(object));
+    printf("\n");
+#endif
+
+    switch (object->type)
+    {
+    case OBJ_CLOSURE:
+    {
+        ObjClosure *closure = (ObjClosure *)object;
+        markObject((Obj *)closure->function);
+        for (int i = 0; i < closure->upvalueCount; i++)
+        {
+            markObject((Obj *)closure->upvalues[i]);
+        }
+        break;
+    }
+    case OBJ_FUNCTION:
+    {
+        ObjFunction *function = (ObjFunction *)object;
+        markObject((Obj *)function->name);
+        markArray(&function->chunk.constants);
+        break;
+    }
+    case OBJ_UPVALUE:
+        markValue(((ObjUpvalue *)object)->closed);
+        break;
+    case OBJ_NATIVE:
+    case OBJ_STRING:
+        break;
+    }
+}
+
 static void freeObject(Obj *object)
 {
 #ifdef DEBUG_LOG_GC
@@ -79,6 +123,9 @@ void markObject(Obj *object)
 {
     if (object == NULL)
         return;
+    if (object->isMarked)
+        return;
+
 #ifdef DEBUG_LOG_GC
     printf("%p mark ", (void *)object);
     printValue(OBJ_VAL(object));
@@ -121,6 +168,15 @@ static void markRoots()
     markCompilerRoots();
 }
 
+static void traceReferences()
+{
+    while (vm.grayCount > 0)
+    {
+        Obj *object = vm.grayStack[--vm.grayCount];
+        blackenObject(object);
+    }
+}
+
 void collectGarbage()
 {
 #ifdef DEBUG_LOG_GC
@@ -128,6 +184,7 @@ void collectGarbage()
 #endif
 
     markRoots();
+    traceReferences();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
