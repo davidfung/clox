da4fa760d048cc6583b721db9b11cdc3d4f2bde6
davidfung <davidfung@amgcomputing.com>
2023-04-03 21:19:56 -0700

    Next 27

--- object.c
+++ object.c
@@ -20,7 +20,7 @@ static Obj *allocateObject(size_t size, ObjType type)
     vm.objects = object;
 
 #ifdef DEBUG_LOG_GC
-  printf("%p allocate %zu for %d\n", (void*)object, size, type);
+    printf("%p allocate %zu for %d\n", (void *)object, size, type);
 #endif
 
     return object;
@@ -65,7 +65,11 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash)
     string->length = length;
     string->chars = chars;
     string->hash = hash;
+
+    push(OBJ_VAL(string));
     tableSet(&vm.strings, string, NIL_VAL);
+    pop();
+
     return string;
 }
 
