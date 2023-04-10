b3f7e3f04c6cc441e872a68df22135dc2ab1af5b
davidfung <davidfung@amgcomputing.com>
2023-04-10 16:53:51 -0700

    Next 27.3

--- compiler.c
+++ compiler.c
@@ -748,6 +748,19 @@ static void function(FunctionType type)
     }
 }
 
+static void classDeclaration()
+{
+    consume(TOKEN_IDENTIFIER, "Expect class name.");
+    uint8_t nameConstant = identifierConstant(&parser.previous);
+    declareVariable();
+
+    emitBytes(OP_CLASS, nameConstant);
+    defineVariable(nameConstant);
+
+    consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+    consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
+}
+
 static void funDeclaration()
 {
     uint8_t global = parseVariable("Expect function name.");
@@ -926,7 +939,11 @@ static void synchronize()
 
 static void declaration()
 {
-    if (match(TOKEN_FUN))
+    if (match(TOKEN_CLASS))
+    {
+        classDeclaration();
+    }
+    else if (match(TOKEN_FUN))
     {
         funDeclaration();
     }
@@ -996,10 +1013,12 @@ ObjFunction *compile(const char *source)
     return parser.hadError ? NULL : function;
 }
 
-void markCompilerRoots() {
-    Compiler* compiler = current;
-    while (compiler != NULL) {
-        markObject((Obj*)compiler->function);
+void markCompilerRoots()
+{
+    Compiler *compiler = current;
+    while (compiler != NULL)
+    {
+        markObject((Obj *)compiler->function);
         compiler = compiler->enclosing;
     }
 }
