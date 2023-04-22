30f5324dd71a4692e50e2d89d517ef4b4200aa94
davidfung <davidfung@amgcomputing.com>
2023-04-22 15:02:30 -0700

    Next 28.3

--- memory.c
+++ memory.c
@@ -61,6 +61,13 @@ static void blackenObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_BOUND_METHOD:
+    {
+        ObjBoundMethod *bound = (ObjBoundMethod *)object;
+        markValue(bound->receiver);
+        markObject((Obj *)bound->method);
+        break;
+    }
     case OBJ_CLASS:
     {
         ObjClass *klass = (ObjClass *)object;
@@ -109,6 +116,9 @@ static void freeObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_BOUND_METHOD:
+        FREE(ObjBoundMethod, object);
+        break;
     case OBJ_CLASS:
     {
         ObjClass *klass = (ObjClass *)object;
