53cf8971dca11409b9237fd0dcdfc996cd2cff0c
davidfung <davidfung@amgcomputing.com>
2023-05-04 21:22:06 -0700

    Next 28.4

--- compiler.c
+++ compiler.c
@@ -75,8 +75,14 @@ typedef struct Compiler
     int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler
+{
+    struct ClassCompiler *enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler *current = NULL;
+ClassCompiler *currentClass = NULL;
 
 static Chunk *currentChunk()
 {
@@ -601,6 +607,12 @@ static void variable(bool canAssign)
 
 static void this_(bool canAssign)
 {
+    if (currentClass == NULL)
+    {
+        error("Can't use 'this' outside of a class.");
+        return;
+    }
+
     variable(false);
 }
 
@@ -798,6 +810,10 @@ static void classDeclaration()
     emitBytes(OP_CLASS, nameConstant);
     defineVariable(nameConstant);
 
+    ClassCompiler classCompiler;
+    classCompiler.enclosing = currentClass;
+    currentClass = &classCompiler;
+
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
     while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF))
@@ -806,6 +822,8 @@ static void classDeclaration()
     }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+
+    currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration()
